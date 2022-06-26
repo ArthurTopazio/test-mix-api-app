9a6f847a2ca44d2ae612b5c032f55f08904b3d40
ArthurTopazio <perereacmb@gmail.com>
2022-06-26 16:29:53 +0300

    add tabs to home page

--- src/components/Content/HomePage/HomePage.tsx
+++ src/components/Content/HomePage/HomePage.tsx
@@ -1,19 +1,32 @@
+import * as React from 'react'
+import Box from '@mui/material/Box'
+import Tabs from '@mui/material/Tabs'
+import Tab from '@mui/material/Tab'
+
 import style from './HomePage.module.scss'
 import headerImg from '../../../assets/images/header-photos/tech1.webp'
 import HeaderWall from '../../HeaderWall/HeaderWall'
 
 //HeaderImage add to props
 const HomePage = () => {
+  const [value, setValue] = React.useState(0)
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue)
+  }
+
   return (
     <div className={style.content__wrapper}>
       <HeaderWall headerImg={headerImg} />
       <div className={style.content}>
-        <div>My Resume</div>
-        <div>push test2</div>
-        <div>text color test</div>
-        <div>text end test</div>
-        <div>text end test</div>
-        <div>lorem*20</div>
+        <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
+          <Tabs value={value} onChange={handleChange} centered>
+            <Tab label="Item One" />
+            <Tab label="Item Two" />
+            <Tab label="Item Three" />
+          </Tabs>
+        </Box>
+        <div>{value}</div>
       </div>
     </div>
   )
