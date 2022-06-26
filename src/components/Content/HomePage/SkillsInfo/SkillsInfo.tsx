f51868910bd25dc31d800a1d42d0155df24021a2
ArthurTopazio <perereacmb@gmail.com>
2022-06-26 19:15:04 +0300

    skills updates

--- src/components/Content/HomePage/SkillsInfo/SkillsInfo.tsx
+++ src/components/Content/HomePage/SkillsInfo/SkillsInfo.tsx
@@ -1,9 +1,15 @@
 import SkillsInfoCard from "./SkillsInfoCard/SkillsInfoCard"
+import style from './SkillsInfo.module.scss'
 
 function SkillsInfo() {
   return (
-    <div>Skills info
-      <SkillsInfoCard />
+    <div className={style.wrapper}>
+      <h2 className={style.topic}>SKILLS</h2>
+      <div className={style.cards__box}>
+        <SkillsInfoCard />
+        <SkillsInfoCard />
+        <SkillsInfoCard />
+      </div>
     </div>
   )
 }
--- src/components/Content/HomePage/SkillsInfo/SkillsInfoCard/SkillsInfoCard.tsx
+++ src/components/Content/HomePage/SkillsInfo/SkillsInfoCard/SkillsInfoCard.tsx
@@ -1,7 +1,15 @@
+import style from './SkillsInfoCard.module.scss'
 
 const SkillsInfoCard = () => {
   return (
-    <div> Card</div>
+    <div className={style.card}>
+      <div className={style.h1}>HTML/CSS </div>
+      <div className={style.skill__items}>
+        <p className={style.skill__item}>-	adaptive layout</p>
+        <p className={style.skill__item}>-	manual style settings</p>
+        <p className={style.skill__item}>-	CSS basic animation </p>
+      </div>
+    </div>
   )
 }
 
