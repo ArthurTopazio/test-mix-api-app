8b0cd196ff482a730891ed9afdb1c6386a006e9e
ArthurTopazio <perereacmb@gmail.com>
2022-06-28 21:30:48 +0300

    header init prototype burger-menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,26 @@
-import style from './Header.module.scss';
+import { NavLink } from 'react-router-dom'
+
+import style from './Header.module.scss'
 
 const Header = () => {
   return <div className={style.header}>
-    <ul className={style.nav}>
-      <li>nav</li>
-      <li>nav</li>
-      <li>nav</li>
-      <li>nav</li>
-      <li>nav</li>
-      <li>nav</li>
-    </ul>
+    <div className={style.wrapper}>
+      <div className={style.logo}>
+        Logo
+      </div>
+      <ul className={style.nav}>
+        <NavLink to={'/news'}
+        >News</NavLink>
+        <NavLink to={'/form'}
+        >Form</NavLink>
+        <li>Dogs</li>
+        <NavLink to={''}
+        >Home</NavLink>
+      </ul>
+      <div className={style.burger__menu}>
+        Burger menu
+      </div>
+    </div>
   </div>
 }
 
