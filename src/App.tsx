df04fd68aa80d62b575bd3874e3ea4e309406e2f
ArthurTopazio <perereacmb@gmail.com>
2022-06-28 20:08:02 +0300

    footer layout init

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"
 
-import './App.scss';
-import HomePage from './components/Content/HomePage/HomePage';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
-import News from './components/Content/News/News';
-import HeaderBannerRoutes from './components/HeaderBanerRoutes/HeaderBannerRoutes';
-import FormPage from './components/Content/FormPage/FormPage';
+import './App.scss'
+import HomePage from './components/Content/HomePage/HomePage'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+import News from './components/Content/News/News'
+import HeaderBannerRoutes from './components/HeaderBanerRoutes/HeaderBannerRoutes'
+import FormPage from './components/Content/FormPage/FormPage'
 
 function App() {
   return (
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,30 @@
+import { Routes, Route } from "react-router-dom"
+
 import style from './Footer.module.scss'
+
+const FooterElement = (props: any) => {
+  console.log(props)
+  return (
+    <div className={!props.style ? style.wrapper : style.wrapperAlt} >
+      <div className={style.box}>
+        <div className={style.item}>my profile</div>
+        <div className={style.item}>open sorses</div>
+        <div className={style.item}>partneres</div>
+      </div>
+    </div>
+  )
+}
 
 const Footer = () => {
   return (
-    <div className={style.layout} >
-      Footer
-    </div>
+    <Routes>
+      <Route path=""
+        element={<FooterElement style={false} />} />
+      <Route path="/news"
+        element={<FooterElement style={false} />} />
+      <Route path="/form"
+        element={<FooterElement style={true} />} />
+    </Routes>
   )
 }
 
