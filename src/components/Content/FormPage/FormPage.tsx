cf33fe39c55b43978ac5ee2fb010d556aa3d12a2
ArthurTopazio <perereacmb@gmail.com>
2022-06-30 17:19:23 +0300

    update dogs-list request & layout

--- src/components/Content/FormPage/FormPage.tsx
+++ src/components/Content/FormPage/FormPage.tsx
@@ -2,6 +2,97 @@ import { Formik, Form, Field, ErrorMessage } from 'formik'
 import * as Yup from 'yup'
 
 import style from './FormPage.module.scss'
+import { styled } from '@mui/system';
+import SwitchUnstyled, { switchUnstyledClasses } from '@mui/base/SwitchUnstyled';
+
+const blue = {
+  500: '#007FFF',
+};
+
+const grey = {
+  400: '#BFC7CF',
+  500: '#AAB4BE',
+  600: '#6F7E8C',
+};
+
+const Root = styled('span')(
+  ({ theme }) => `
+  font-size: 0;
+  position: relative;
+  display: inline-block;
+  width: 40px;
+  height: 20px;
+  margin: 10px;
+  cursor: pointer;
+
+  &.${switchUnstyledClasses.disabled} {
+    opacity: 0.4;
+    cursor: not-allowed;
+  }
+
+  & .${switchUnstyledClasses.track} {
+    background: ${theme.palette.mode === 'dark' ? grey[600] : grey[400]};
+    border-radius: 10px;
+    display: block;
+    height: 100%;
+    width: 100%;
+    position: absolute;
+  }
+
+  & .${switchUnstyledClasses.thumb} {
+    display: block;
+    width: 14px;
+    height: 14px;
+    top: 3px;
+    left: 3px;
+    border-radius: 16px;
+    background-color: #fff;
+    position: relative;
+    transition: all 200ms ease;
+  }
+
+  &.${switchUnstyledClasses.focusVisible} .${switchUnstyledClasses.thumb} {
+    background-color: ${grey[500]};
+    box-shadow: 0 0 1px 8px rgba(0, 0, 0, 0.25);
+  }
+
+  &.${switchUnstyledClasses.checked} {
+    .${switchUnstyledClasses.thumb} {
+      left: 22px;
+      top: 3px;
+      background-color: #fff;
+    }
+
+    .${switchUnstyledClasses.track} {
+      background: ${blue[500]};
+    }
+  }
+
+  & .${switchUnstyledClasses.input} {
+    cursor: inherit;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    opacity: 0;
+    z-index: 1;
+    margin: 0;
+  }
+  `,
+);
+
+//switchMUI component
+export function UnstyledSwitches() {
+  const label = { componentsProps: { input: { 'aria-label': 'Demo switch' } } };
+
+  return (
+    <div>
+      <SwitchUnstyled component={Root} {...label} />
+    </div>
+  );
+}
+
 
 //yup method
 const validationSchema = Yup.object().shape({
@@ -18,6 +109,12 @@ const validationSchema = Yup.object().shape({
   email: Yup.string()
     .email('Invalid email')
     .required('I cant get feedback without email'),
+  password: Yup.string()
+    .min(2, 'Too short for password')
+    .max(8, 'Too lang, max 8 symbols')
+    .required('Please write password'),
+  confirmPassword: Yup.string().oneOf([Yup.ref('password')], 'passwords incorrect')
+    .required('Password is incorrect'),
 })
 
 //example with yup
@@ -35,6 +132,8 @@ const FormExample = () => (
         message: '',
         colors: '',
         acceptedTerms: false,
+        password: '',
+        confirmPassword: '',
       }}
       validationSchema={validationSchema}
       onSubmit={values => {
@@ -85,6 +184,17 @@ const FormExample = () => (
           <label htmlFor={'acceptedTerms'}>accept terms</label>
           <Field name='acceptedTerms' type="checkbox" />
           <br />
+          <label htmlFor={'password'}>Enter Your password</label><br />
+          <Field name="password" type='password' />
+          {errors.password && touched.password ? (
+            <div>{errors.password}</div>
+          ) : null} <br />
+          <label htmlFor={'confirmPassword'}>Confirm password</label><br />
+          <Field name="confirmPassword" type='password' />
+          {errors.confirmPassword && touched.confirmPassword ? (
+            <div>{errors.confirmPassword}</div>
+          ) : null} <br />
+          <UnstyledSwitches />
           <button type="submit">Submit</button>
         </Form>
       )}
