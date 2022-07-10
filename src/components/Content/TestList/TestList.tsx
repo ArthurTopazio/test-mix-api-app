ca26493170b5fdcbad795388390450089e1b76b6
ArthurTopazio <perereacmb@gmail.com>
2022-07-10 17:18:48 +0300

    mui test

--- src/components/Content/TestList/TestList.tsx
+++ src/components/Content/TestList/TestList.tsx
@@ -1,10 +1,13 @@
 import style from './TestList.module.scss'
+import * as React from 'react';
+import Button from '@mui/material/Button';
 
 const TestList = (props: any) => {
   return (
     <div className={style.content__wrapper}>
       <div className={style.content}>
         <div className={style.text}>Test area</div>
+        <Button variant="contained" onClick={() => { alert('hi') }}>Hello World</Button>
       </div>
     </div>
   )
